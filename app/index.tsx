a188a69f62840dab942f0d3316424fe1b9d84676
Tharusha-Delpachitra <tharushadelpa@gmail.com>
2024-12-20 10:13:05 +0530

    Initialized the project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css";
+
+export default function index() {
+  return (
+    <View>
+        <Text className='bg-blue-500 text-white text-3xl text-center font-bold'>Hello world</Text>
+    </View>
+  )
+}
+
